b5f374f29cdcc6de267e211ada433fdf9736b2f9
kumaneko <n_shichi4279@outlook.jp>
2018-04-15 01:19:45 +0900

    とりあえず全部コミットする

--- OpemGLESFragment/app/src/main/java/com/example/student/openglesandroid/MyGLSurfaceView.java
+++ OpemGLESFragment/app/src/main/java/com/example/student/openglesandroid/MyGLSurfaceView.java
@@ -1,8 +1,24 @@
 package com.example.student.openglesandroid;
 
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
 /**
  * Created by student on 2018/04/15.
  */
+class MyGLSurfaceView extends GLSurfaceView {
 
-class MyGLSurfaceView {
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // OpenGL ES 2.0コンテキストを作成する
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // GLSurfaceViewで描画するためのレンダラーを設定
+        setRenderer(mRenderer);
+    }
 }
