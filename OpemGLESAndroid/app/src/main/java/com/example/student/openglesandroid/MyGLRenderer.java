3c4f2b489b30f7fc54269169964418ad559952db
kumaneko <n_shichi4279@outlook.jp>
2018-04-15 21:21:11 +0900

    回転で形が崩れるのを修正

--- OpemGLESAndroid/app/src/main/java/com/example/student/openglesandroid/MyGLRenderer.java
+++ OpemGLESAndroid/app/src/main/java/com/example/student/openglesandroid/MyGLRenderer.java
@@ -2,6 +2,7 @@ package com.example.student.openglesandroid;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -33,12 +34,31 @@ class MyGLRenderer implements GLSurfaceView.Renderer{
     public void onDrawFrame(GL10 unused) {
         //背景色の再描画
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw shape
+        mTriangle.draw(mMVPMatrix);
     }
-    //Viewのジオメトリが変更された際に呼び出し（デバイスの向き変更など）
+
+    private final float[] mMVPMatrix = new float[16];// "Model View Projection Matrix"の略
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+    //Viewのジオメトリが変更された際に呼び出し（デバイスの向き変更など）。
+    //投影の定義
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0,0,width,height);
+
+        float ratio =(float) width / height;
+
+        ///この投影行列はオブジェクトの座標に適用。onDrawFame（）メソッド内。
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
