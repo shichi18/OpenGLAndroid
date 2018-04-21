96aaf49094bad6a88d463d483cbd85f2cdf0878b
kumaneko <n_shichi4279@outlook.jp>
2018-04-21 18:01:52 +0900

    残りのコミット

--- CubeAndroid/app/src/main/java/com/example/student/cubeandroid/MyGLRenderer.java
+++ CubeAndroid/app/src/main/java/com/example/student/cubeandroid/MyGLRenderer.java
@@ -2,6 +2,7 @@ package com.example.student.cubeandroid;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -23,15 +24,28 @@ class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 unused) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
-        mCube.draw();
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw shape
+        mCube.draw(mMVPMatrix);
 
     }
+
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
 
     //縦横時の切り替え時に呼ばれる
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
